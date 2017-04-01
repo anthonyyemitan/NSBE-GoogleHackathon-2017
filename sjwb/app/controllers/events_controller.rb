a8ffcee93790230216cc2d4becbac5c80185fd95
William Thurman <William Thurman>
2017-04-01 02:43:04 -0500

    Working on controller functionality. Added html skeleton

--- sjwb/app/controllers/events_controller.rb
+++ sjwb/app/controllers/events_controller.rb
@@ -1,19 +1,24 @@
 class EventsController < ApplicationController
 
   def new
-
+    
   end
 
   def create
+    @event = Event.new(params[:event])
 
+    if @event.save
+      redirect_to @event
+    else
+      render 'errors\save_error'
   end
 
   def list
-
+    @events = Event.all
   end
 
   def show
-    
+    @event = Event.find(params[:id])
   end
 
 end
