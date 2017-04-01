45baa57b77d968005ebe07bd7bb44cc696ae561e
William Thurman <William Thurman>
2017-04-01 06:42:23 -0500

    Changing a link

--- sjwb/config/routes.rb
+++ sjwb/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   get 'events/', to: 'events#list'
   get 'events/create', to: 'events#new'
-  get 'events/:id', to: 'events#show'
   post 'events/', to: 'events#create'
+  get 'events/:id', to: 'events#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
